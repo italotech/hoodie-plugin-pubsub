b474091cccf02b096b791f73497206131881ab4e
João Neto <joaopintoneto@gmail.com>
2014-11-10 18:10:05 -0200

    feat(ensureUserFilter) filtered replication

--- worker.js
+++ worker.js
@@ -8,8 +8,6 @@ var async = require('async');
 module.exports = function (hoodie, callback) {
   var PubSub = require('./lib/pubsub')(hoodie);
 
-  console.log('PUBSUB INITIALIZED');
-
   hoodie.task.on('subscribe:add', function (db, task) {
     var subject = task.subject;
     var sourceDbName = 'user/' + task.userId;
@@ -19,15 +17,11 @@ module.exports = function (hoodie, callback) {
       return hoodie.task.error(db, task, 'Pls, fill the params: userId and subject');
     }
 
-    try {
-      PubSub.subscribe(subject, sourceDbName, targetDbName, function (err, req) {
-        console.log(err && err.error)
-        if (err) return hoodie.task.error(db, task, err.error || err);
-        hoodie.task.success(db, task);
-      });
-    } catch (err) {
-      hoodie.task.error(db, task, err.error || err);
-    }
+    PubSub.subscribe(subject, sourceDbName, targetDbName, function (err, req) {
+      // console.log(err && err.error)
+      if (err) return hoodie.task.error(db, task, err.error || err);
+      hoodie.task.success(db, task);
+    });
   });
 
   hoodie.task.on('unsubscribe:add', function (db, task) {
@@ -39,26 +33,27 @@ module.exports = function (hoodie, callback) {
       return hoodie.task.error(db, task, 'Pls, fill the params: userId and subject');
     }
 
-    try {
-      PubSub.unsubscribe(subject, sourceDbName, targetDbName, function (err, req) {
-        console.log(err && err.error)
-        if (err) return hoodie.task.error(db, task, err.error || err);
-        hoodie.task.success(db, task);
-      });
-    } catch (err) {
-      hoodie.task.error(db, task, err.error || err);
-    }
+    PubSub.unsubscribe(subject, sourceDbName, targetDbName, function (err, req) {
+      // console.log(err && err.error)
+      if (err) return hoodie.task.error(db, task, err.error || err);
+      hoodie.task.success(db, task);
+    });
   });
 
   hoodie.task.on('publish:add', function (db, task) {
     console.log('publish:add', task);
 
-    try {
-      hoodie.task.success(db, task);
-    } catch (err) {
-      hoodie.task.error(db, task, err);
-    }
+    hoodie.task.success(db, task);
+    // hoodie.task.error(db, task, err);
   });
+
+  // hoodie.account.on('change', function (_doc) {
+  //   if (_doc.roles && _doc.roles.indexOf('confirmed') >= 0) {
+  //     PubSub.ensureUserFilter(_doc.hoodieId, function (err, _doc) {
+  //       if (err) console.error('PubSub.ensureUserFilter:', err);
+  //     });
+  //   }
+  // });
 
   async.series([
     async.apply(PubSub.addDb),
