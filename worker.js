2683df7cfbe431357ff18a6e1e4aec838a37d538
Gabriel Mancini <gabriel.mancini@gmail.com>
2014-11-11 19:05:36 -0200

    refactoring

--- worker.js
+++ worker.js
@@ -3,43 +3,21 @@
  * Lightweight and easy pubsub
  */
 
-var async = require('async');
+var async = require('async'),
+    PubSub = require('./lib/pubsub'),
+    utisl = require('./lib/utils'),
+    _ = require('lodash');
+
+exports.dbname = 'plugins/hoodie-plugin-pubsub';
 
 module.exports = function (hoodie, callback) {
-  var PubSub = require('./lib/pubsub')(hoodie);
-  var Design = require('./lib/design')(hoodie);
 
-  hoodie.task.on('subscribe:add', function (db, task) {
-    var subject = task.subject;
-    var sourceDbName = 'user/' + task.userId;
-    var targetDbName = db;
+    var pluginDb = hoodie.database(exports.dbname),
+      pubsub = new PubSub(hoodie, pluginDb);
 
-    if (!task.userId || !subject) {
-      return hoodie.task.error(db, task, 'Pls, fill the params: userId and subject');
-    }
+  hoodie.task.on('subscribe:add', pubsub.subscribe);
+  hoodie.task.on('unsubscribe:add', pubsub.unsubscribe);
 
-    PubSub.subscribe(subject, sourceDbName, targetDbName, function (err, req) {
-      // console.log(err && err.error)
-      if (err) return hoodie.task.error(db, task, err.error || err);
-      hoodie.task.success(db, task);
-    });
-  });
-
-  hoodie.task.on('unsubscribe:add', function (db, task) {
-    var subject = task.subject;
-    var sourceDbName = 'user/' + task.userId;
-    var targetDbName = db;
-
-    if (!task.userId || !subject) {
-      return hoodie.task.error(db, task, 'Pls, fill the params: userId and subject');
-    }
-
-    PubSub.unsubscribe(subject, sourceDbName, targetDbName, function (err, req) {
-      // console.log(err && err.error)
-      if (err) return hoodie.task.error(db, task, err.error || err);
-      hoodie.task.success(db, task);
-    });
-  });
 
   hoodie.task.on('publish:add', function (db, task) {
     console.log('publish:add', task);
@@ -49,20 +27,128 @@ module.exports = function (hoodie, callback) {
   });
 
   hoodie.account.on('change', function (_doc) {
-    console.log(_doc.roles)
-    if (_doc.roles && _doc.roles.indexOf('confirmed') >= 0) {
-      Design.ensureUserFilter(_doc.hoodieId, function (err, _doc) {
-        if (err) console.error('PubSub.ensureUserFilter:', err);
-      });
+    if(_doc.type && _doc.type === 'user') {
+      var userDbName = 'user/' + _doc.hoodieId;
+      var userDb = hoodie.database(userDbName);
+      async.series([
+          async.apply(extendDb, hoodie, userDb),
+          async.apply(exports.dbFilter, hoodie, userDb)
+        ],
+        function (err, _doc) {
+          if (err) console.error('PubSub.ensureUserFilter:', err);
+        }
+      )
     }
   });
 
   async.series([
-    async.apply(PubSub.addDb),
+    async.apply(exports.dbAdd, hoodie),
   ],
   callback);
 };
 
+exports.dbAdd = function (hoodie, callback) {
+
+  hoodie.database.add(exports.dbname, function (err) {
+
+    if (err) {
+      return callback(err);
+    }
+
+    return callback();
+
+  });
+
+};
+
+exports.dbFilter = function (hoodie, pluginDb, callback) {
+  var filter = function (doc, req) {
+    if (doc.type == req.query.id) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  pluginDb.addFilter('pubsub', 'by_type', filter, function (err, data) {
+    if (err) {
+      return callback(err);
+    }
+
+    return callback();
+  })
+};
 
 
+//TODO: Pullrequest
+function extendDb(hoodie, pluginDb, cb) {
+  var db = pluginDb;
+/**
+   * CouchDB views created using `db.addIndex()` are all stored in the same
+   * design document: `_design/views`.
+   * See https://github.com/hoodiehq/hoodie.js/issues/70#issuecomment-20506841
+   */
 
+
+  /**
+   * Creates new design doc with CouchDB view on db
+   */
+
+  db.addFilter = function (filterName, name, filter, callback) {
+    var views_ddoc_id = '_design/' + filterName;
+    var views_ddoc_url = db._resolve(views_ddoc_id);
+
+    hoodie.request('GET', views_ddoc_url, {}, function (err, ddoc, res) {
+      if (res.statusCode === 404) {
+        // not found, so we use new object.
+        ddoc = {
+          language: 'javascript',
+          filters: {}
+        };
+      } else if (err) {
+        return callback(err);
+      }
+
+      // View functions need to be serialised/stringified.
+      var serialised = filter.toString();
+
+      // If view code has not changed we don't need to do anything else.
+      // NOTE: Not sure if this is the best way to deal with this. This
+      // saves work and avoids unnecessarily overwriting the
+      // `_design/views` document when no actual changes have been made to
+      // the view code (map/reduce).
+      if (_.isEqual(serialised, ddoc.filters[name])) {
+        return callback(null, {
+          ok: true,
+          id: ddoc._id,
+          rev: ddoc._rev
+        });
+      }
+
+      ddoc.filters[name] = serialised;
+
+      hoodie.request('PUT', views_ddoc_url, {data: ddoc}, callback);
+    });
+  };
+
+  /**
+   * Removes couchdb view from db
+   */
+
+  db.removeIndex = function (filterName, name, callback) {
+    var views_ddoc_id = '_design/' + filterName;
+    var views_ddoc_url = db._resolve(views_ddoc_id);
+    hoodie.request('GET', views_ddoc_url, {}, function (err, ddoc) {
+      if (err) {
+        return callback(err);
+      }
+
+      if (ddoc.filters && ddoc.filters[name]) {
+        delete ddoc.filters[name];
+      }
+
+      hoodie.request('PUT', views_ddoc_url, {data: ddoc}, callback);
+    });
+  };
+  cb();
+};
