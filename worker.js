8a6f1200fda8bb5c9b3850240fdf00e9560723b2
Gabriel Mancini <gabriel.mancini@gmail.com>
2014-11-14 17:59:58 -0200

    refactor

--- worker.js
+++ worker.js
@@ -6,141 +6,24 @@
 /**
  * Dependencies
  */
-
-var async = require('async');
-var _ = require('underscore');
-var PubSub = require('./lib/pubsub');
-var Network = require('./lib/network');
-var ExtendedDatabaseAPI = require('./lib/extended-database').ExtendedDatabaseAPI;
-
-/**
- * DevDependencies
- */
-
-var debug = require('debug');
-var log = debug('app:log');
-var error = debug('app:error');
-
-
-/**
- * PubSub dbName
- */
-
-exports.dbname = 'plugins/hoodie-plugin-pubsub';
-
-/**
- * PubSub dbAdd
- */
-
-exports.dbAdd = function (hoodie, callback) {
-  hoodie.database.add(exports.dbname, function (err) {
-    if (err) error(err);
-    callback(err);
-  });
-};
-
-/**
- * PubSub userFilter
- */
-
-exports.userFilter = function (hoodie, db, callback) {
-  var filterFunction = function (doc, req) {
-    if (doc.type == req.query.type) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  db.addFilter('by_type', filterFunction, function (err, data) {
-    if (err) {
-      return callback(err);
-    }
-
-    return callback();
-  })
-};
-
-
-/**
- * PubSub addIndexSubscribers
- */
-
-exports.addIndexSubscribers = function (hoodie, db, callback) {
-
-  var index = {
-        map: function (doc) {
-          if(doc.target) {
-            emit(doc.target, doc._id);
-          }
-        }
-      };
-
-  db.addIndex('by_subscribers', index, function (err, data) {
-    if (err) {
-      return callback(err);
-    }
-
-    return callback();
-  })
-};
-
-/**
- * PubSub addIndexSubscribers
- */
-
-exports.addIndexSubscriptions = function (hoodie, db, callback) {
-
-  var index = {
-        map: function (doc) {
-          if(doc.source) {
-            emit(doc.source, doc._id);
-          }
-        }
-      };
-
-  db.addIndex('by_subscriptions', index, function (err, data) {
-    if (err) {
-      return callback(err);
-    }
-
-    return callback();
-  })
-};
+var PubSub = require('./lib/index');
 
 /**
  * PubSub worker
  */
 
 module.exports = function (hoodie, callback) {
-  var pluginDb = new ExtendedDatabaseAPI(hoodie, hoodie.database(exports.dbname));
-  var pubSub = new PubSub(hoodie, pluginDb);
-  var network = new Network(hoodie, pluginDb);
+
+  var pubSub = new PubSub(hoodie);
 
   hoodie.task.on('subscribe:add', pubSub.subscribe);
   hoodie.task.on('unsubscribe:add', pubSub.unsubscribe);
-  hoodie.task.on('subscribers:add', network.subscribers);
-  hoodie.task.on('subscriptions:add', network.subscriptions);
+  hoodie.task.on('subscribers:add', pubSub.subscribers);
+  hoodie.task.on('subscriptions:add', pubSub.subscriptions);
 
   // hoodie.task.on('publish:add', pubSub.publish);
-  hoodie.account.on('change', function (_doc) {
-    if (_doc.roles && _doc.roles.indexOf('confirmed') >= 0) {
-      var userDbName = 'user/' + _doc.hoodieId;
-      var userDb = new ExtendedDatabaseAPI(hoodie, hoodie.database(userDbName));
+  hoodie.account.on('change', pubSub.addFilterEachUser);
 
-      async.series([
-        async.apply(exports.userFilter, hoodie, userDb)
-      ],
-      function (err, _doc) {
-        if (err) error('PubSub.ensureUserFilter:', err);
-      });
-    }
-  });
+  callback();
 
-  async.series([
-    async.apply(exports.dbAdd, hoodie),
-    async.apply(exports.addIndexSubscribers, hoodie, pluginDb),
-    async.apply(exports.addIndexSubscriptions, hoodie, pluginDb),
-  ],
-  callback);
 };
